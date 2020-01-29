a7515042e5c898e7772bf023bf364c78fd57c5a2
jschlitt1 <47009980+jschlitt1@users.noreply.github.com>
2020-01-28 21:01:32 -0600

    update before restarting inteliJ

--- src/main/java/edu/wctc/ListServlet.java
+++ src/main/java/edu/wctc/ListServlet.java
@@ -6,11 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-@WebServlet(name = "ListServlet")
+@WebServlet(name = "ListServlet", urlPatterns = "/ClassList")
 public class ListServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
@@ -18,16 +16,84 @@ public class ListServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
         try {
+            //create connections
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 
             String absPath = getServletContext().getRealPath("/") + "../../db";
 
             absPath ="jdbc:derby:" + absPath;
 
-            Connection conn = DriverManager.getConnection(absPath, "jessie", "jessie");
+            conn = DriverManager.getConnection(absPath, "jessie", "jessie");
+
+            stmt = conn.createStatement();
+
+            //execute statements
+
+            rset = stmt.executeQuery("select Title, Publisher, Developer from Games");
+
+            StringBuilder sb = new StringBuilder("<html><body><table><tr><th>Title</th><th>Publisher</th><th>Developer</th></tr>");
+
+            while (rset.next()){
+                String Title = rset.getString("Title");
+                int Publisher = rset.getInt("Publisher");
+                int Developer = rset.getInt("Developer");
+
+                sb.append("<tr>");
+
+                sb.append("<td>");
+                sb.append(Title);
+                sb.append("</td>");
+
+                sb.append("<td>");
+                sb.append(Publisher);
+                sb.append("</td>");
+
+                sb.append("<td>");
+                sb.append(Developer);
+                sb.append("</td>");
+
+                sb.append("</tr>");
+            }
+            sb.append("</table></body></html>");
+
+            response.setContentType("text/html");
+
+            response.getWriter().print(sb.toString());
+
+            //close the connection: see the finally
+
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        } finally{
+            closeAll(conn, stmt, rset);
+        }
+    }
+
+    public static void closeAll(Connection conn, Statement stmt, ResultSet rset) {
+        if (rset != null) {
+            try {
+                rset.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
