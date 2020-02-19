75a6ae1ef42c22596fbe124d18f98c9c9aba17ea
jschlitt1 <47009980+jschlitt1@users.noreply.github.com>
2020-02-18 20:52:38 -0600

    Extra Notes

--- src/main/java/edu/wctc/ListServlet.java
+++ src/main/java/edu/wctc/ListServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.*;
-
+//This was for the class example, the one currently in use is MainServlet on the links
 @WebServlet(name = "ListServlet", urlPatterns = "/ClassList")
 public class ListServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
